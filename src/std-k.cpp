8362e8dbb14b0f6cd5d5311f61aaca3b8123fc20
kiddersmxj <maxkiddmay@gmail.com>
2023-05-08 20:32:22 +0100

    added namespace to map

--- src/std-k.cpp
+++ src/std-k.cpp
@@ -101,7 +101,7 @@ int k::VGetIndex(std::vector<std::string> v, std::string K) {
     }
 }
 
-int Map(int x, int in_min, int in_max, int out_min, int out_max) {
+int k::Map(int x, int in_min, int in_max, int out_min, int out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 

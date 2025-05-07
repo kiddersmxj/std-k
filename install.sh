d258232bb33cc08576794f67c14228d3cb226a04
kiddersmxj <maxkiddmay@gmail.com>
2025-05-07 16:22:19 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-if [[ $1 != "" ]]; then
-    ARGS="-D$1"
+if [[ 1 != "" ]]; then
+    ARGS="-D1"
 fi
 
-cmake -B build $ARGS && cmake --build build && sudo cmake --install build
+cmake -B build ARGS && cmake --build build && sudo cmake --install build
+
+# Copyright (c) 2023, Maxamilian Kidd-May
+# All rights reserved.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree. 
 
